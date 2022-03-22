722dc630a79934ebc2260eb64c0fdbc008cb5628
himabhavyasri <himabhavyasri@gmail.com>
2022-03-22 20:29:37 +0530

    commit2

--- src/App.js
+++ src/App.js
@@ -1,51 +1,82 @@
-import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className='bg-container'>
-      <div className='calculator'>
-        <div>
-          <input type="text" className="input"></input>
+class App extends Component {
+
+  state = {
+    result : ' ',
+  }
+
+  updateText = event => {
+    const value = event.target.value;
+    if(value === "=") {
+      try {
+        this.setState((prevState) => ({result: prevState.result + "=" + eval(prevState.result)
+      }))
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+    else if(value === "clear") {
+      this.setState({result : ""})
+    }
+    else if(value === "cancel") {
+      this.setState((prevState) => ({result: prevState.result.slice(0, -1)}))
+    }
+    else {
+      this.setState((prevState) => ({ result: prevState.result + value }));
+    }
+  }
+
+  render () {
+      const {result} = this.state;
+      console.log({result});
+      return (
+        <div className='bg-container'>
+          <div className='calculator'>
+            <div>
+              <input type="text" className="input" value={result} onChange=""></input>
+            </div>
+            <div>
+              <button className="button" onClick={this.updateText} value="clear">AC</button>
+              <button className="button" onClick={this.updateText} value="cancel">x</button>
+              <button className='button' onClick={this.updateText} value="%">%</button>
+              <button className='button' onClick={this.updateText} value="/">/</button>
+            </div> 
+            
+            <div>
+              <button className='button' onClick={this.updateText} value="7">7</button>
+              <button className='button' onClick={this.updateText} value="8">8</button>
+              <button className='button' onClick={this.updateText} value="9">9</button>
+              <button className='button' onClick={this.updateText} value="*">*</button>
+            </div>
+            
+            <div>
+              <button className='button' onClick={this.updateText} value="4">4</button>
+              <button className='button' onClick={this.updateText} value="5">5</button>
+              <button className='button' onClick={this.updateText} value="6">6</button>
+              <button className='button' onClick={this.updateText} value="-">-</button>
+            </div>
+            
+            <div>
+              <button className='button' onClick={this.updateText} value="1">1</button>
+              <button className='button' onClick={this.updateText} value="2">2</button> 
+              <button className='button' onClick={this.updateText} value="3">3</button>
+              <button className='button' onClick={this.updateText} value="+">+</button>
+            </div>
+            
+            <div>
+              <button className='button' onClick={this.updateText} value="0">0</button>
+              <button className='button' onClick={this.updateText} value=".">.</button>
+              <button className="enter" onClick={this.updateText}  value="=">=</button>
+            </div>  
+          </div>
         </div>
-        <div>
-          <button className="button">AC</button>
-          <button className="button">x</button>
-          <button className='button'>%</button>
-          <button className='button'>/</button>
-        </div> 
-        
-        <div>
-          <button className='button'>7</button>
-          <button className='button'>8</button>
-          <button className='button'>9</button>
-          <button className='button'>*</button>
-        </div>
-        
-        <div>
-          <button className='button'>4</button>
-          <button className='button'>5</button>
-          <button className='button'>6</button>
-          <button className='button'>-</button>
-        </div>
-        
-        <div>
-          <button className='button'>1</button>
-          <button className='button'>2</button> 
-          <button className='button'>3</button>
-          <button className='button'>+</button>
-        </div>
-        
-        <div>
-          <button className='button'>0</button>
-          <button className='button'>.</button>
-          <button className="enter">=</button>
-        </div>
-        
-      </div>
-    </div>
-  );
+      )
+  }
+  
 }
 
 export default App;
